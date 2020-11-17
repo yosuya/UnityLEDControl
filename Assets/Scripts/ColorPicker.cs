830a69c5d78ee8a8748674a00a045d083e4ab998
yosuya <yosuyamt@gmail.com>
2020-11-18 01:36:11 +0900

    Add: カメラ画像のテクスチャ化を実装

---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yosuya.UnityLEDControl
+{
+    public class ColorPicker
+    {
+        ///<summary>テクスチャの特定の位置の色を取得</summary>
+        static public Color Pick(Texture2D texture2D, Vector2 pos)
+        {
+            if (pos.x < 0.0f || pos.x > 1.0f || pos.y < 0.0f || pos.y > 1.0f)
+            {
+                Debug.LogWarning("0.0 - 1.0 の範囲で入力してください.");
+                return Color.black;
+            }
+
+            return texture2D.GetPixel(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y));
+        }
+    }
+}
---
+++ Assets/Scripts/LEDCam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yosuya.UnityLEDControl
+{
+    [RequireComponent(typeof(Camera))]
+    public class LEDCam : MonoBehaviour
+    {
+        protected RenderTexture renderTex;
+        protected Texture2D tex2d;
+
+        protected Camera _cam;
+
+        void Start()
+        {
+            renderTex = new RenderTexture(1280, 720, 16, RenderTextureFormat.ARGB32);
+            renderTex.Create();
+
+            _cam = GetComponent<Camera>();
+            _cam.targetTexture = renderTex;
+        }
+
+
+        ///<summary>カメラのピクセルデータを読み込む</summary>
+        public Texture2D ReadPixels()
+        {
+            tex2d = new Texture2D(renderTex.width, renderTex.height, TextureFormat.ARGB32, false, false);
+            _cam.Render();
+
+            RenderTexture.active = renderTex;
+            tex2d.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+            tex2d.Apply();
+            return tex2d;
+        }
+    }
+}
