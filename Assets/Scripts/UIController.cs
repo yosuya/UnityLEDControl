abdcbdd1fa88af07c273a41804377bedd9c19ff8
yosuya <yosuyamt@gmail.com>
2020-11-19 00:19:58 +0900

    Add: UIを作成

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    protected RawImage LEDCam_RawImage;
+    protected RawImage WebCam_RawImage;
+
+    protected RenderTexture _ledCamRT;
+    protected RenderTexture _WebCamRT;
+}
---
+++ Assets/Scripts/WebCamController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamController : MonoBehaviour
+{
+    private WebCamTexture webCamTex;
+
+    private void Start()
+    {
+        webCamTex = new WebCamTexture();
+        
+    }
+}
